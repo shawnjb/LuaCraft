b4ad58fe995a10e79250ca92411d77606f64a44b
dankfmemes <dankfmemes@gmail.com>
2024-09-30 09:03:35 -0700

    Entity manipulation

--- src/main/java/com/dankfmemes/luacraft/lib/LuaCraftLibrary.java
+++ src/main/java/com/dankfmemes/luacraft/lib/LuaCraftLibrary.java
@@ -2,7 +2,14 @@ package com.dankfmemes.luacraft.lib;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.VarArgFunction;
 import org.luaj.vm2.Globals;
@@ -10,6 +17,7 @@ import org.luaj.vm2.LuaValue;
 
 import com.dankfmemes.luacraft.LuaCraft;
 import com.dankfmemes.luacraft.utils.Vec3;
+import com.dankfmemes.luacraft.utils.Entities;
 
 import net.kyori.adventure.text.Component;
 
@@ -47,12 +55,12 @@ public class LuaCraftLibrary {
             }
         });
 
-        table.set("wait", new VarArgFunction() {
+        table.set("sleep", new VarArgFunction() {
             @Override
             public Varargs invoke(Varargs args) {
-                int seconds = args.checkint(1);
+                double seconds = args.checkdouble(1);
                 try {
-                    Thread.sleep(seconds * 1000);
+                    Thread.sleep((long) (seconds * 1000));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -60,7 +68,17 @@ public class LuaCraftLibrary {
             }
         });
 
-        table.set("runcommand", new VarArgFunction() {
+        table.set("wait", new VarArgFunction() {
+            @Override
+            public Varargs invoke(Varargs args) {
+                double seconds = args.checkdouble(1);
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                }, (long) (seconds * 20));
+                return LuaValue.NIL;
+            }
+        });
+
+        table.set("runCommand", new VarArgFunction() {
             @Override
             public Varargs invoke(Varargs args) {
                 String command = args.checkjstring(1);
@@ -76,15 +94,16 @@ public class LuaCraftLibrary {
             }
         });
 
-        table.set("runCommand", table.get("runcommand"));
+        table.set("runcommand", table.get("runCommand"));
 
         table.set("getPlayerPosition", new VarArgFunction() {
             @Override
             public Varargs invoke(Varargs args) {
                 if (plugin.getLastSender() instanceof Player) {
                     Player player = (Player) plugin.getLastSender();
-                    Vec3 position = new Vec3(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
-                    
+                    Vec3 position = new Vec3(player.getLocation().getX(), player.getLocation().getY(),
+                            player.getLocation().getZ());
+
                     LuaValue positionTable = LuaValue.tableOf();
                     positionTable.set("x", position.x);
                     positionTable.set("y", position.y);
@@ -92,7 +111,8 @@ public class LuaCraftLibrary {
 
                     return positionTable;
                 } else {
-                    plugin.getLastSender().sendMessage(plugin.translateColorCodes("You must be a player to get position."));
+                    plugin.getLastSender()
+                            .sendMessage(plugin.translateColorCodes("You must be a player to get position."));
                 }
                 return LuaValue.NIL;
             }
@@ -110,7 +130,131 @@ public class LuaCraftLibrary {
 
                     player.teleport(new Location(player.getWorld(), x, y, z));
                 } else {
-                    plugin.getLastSender().sendMessage(plugin.translateColorCodes("You must be a player to set position."));
+                    plugin.getLastSender()
+                            .sendMessage(plugin.translateColorCodes("You must be a player to set position."));
+                }
+                return LuaValue.NIL;
+            }
+        });
+
+        table.set("summonAtPosition", new VarArgFunction() {
+            @Override
+            public Varargs invoke(Varargs args) {
+                String entityName = args.checkjstring(1);
+                double x = args.checkdouble(2);
+                double y = args.checkdouble(3);
+                double z = args.checkdouble(4);
+
+                EntityType entityType;
+                try {
+                    entityType = EntityType.valueOf(entityName.toUpperCase());
+                } catch (IllegalArgumentException e) {
+                    plugin.getLastSender()
+                            .sendMessage(plugin.translateColorCodes("Invalid entity type: " + entityName));
+                    return LuaValue.NIL;
+                }
+
+                if (plugin.getLastSender() instanceof Player) {
+                    Player player = (Player) plugin.getLastSender();
+                    Location location = new Location(player.getWorld(), x, y, z);
+                    Entity entity = player.getWorld().spawnEntity(location, entityType);
+                    plugin.getLastSender().sendMessage(plugin
+                            .translateColorCodes("Summoned " + entityName + " at (" + x + ", " + y + ", " + z + ")."));
+                    return LuaValue.valueOf(entity.getUniqueId().toString());
+                } else {
+                    plugin.getLastSender()
+                            .sendMessage(plugin.translateColorCodes("You must be a player to summon entities."));
+                }
+
+                return LuaValue.NIL;
+            }
+        });
+
+        table.set("getAllEntities", new VarArgFunction() {
+            @Override
+            public Varargs invoke(Varargs args) {
+                JSONArray entityArray = new JSONArray();
+
+                if (plugin.getLastSender() instanceof Player) {
+                    Player player = (Player) plugin.getLastSender();
+                    for (Entity entity : player.getWorld().getEntities()) {
+                        JSONObject entityInfo = new JSONObject();
+                        entityInfo.put("id", entity.getUniqueId().toString());
+                        entityInfo.put("type", entity.getType().toString());
+                        entityInfo.put("position", new JSONObject() {
+                            {
+                                put("x", entity.getLocation().getX());
+                                put("y", entity.getLocation().getY());
+                                put("z", entity.getLocation().getZ());
+                            }
+                        });
+                        entityArray.add(entityInfo);
+                    }
+                    return LuaValue.valueOf(entityArray.toJSONString());
+                } else {
+                    plugin.getLastSender()
+                            .sendMessage(plugin.translateColorCodes("You must be a player to get entity data."));
+                }
+
+                return LuaValue.NIL;
+            }
+        });
+
+        table.set("modifyEntityData", new VarArgFunction() {
+            @Override
+            public Varargs invoke(Varargs args) {
+                if (args.isstring(1) && args.istable(2)) {
+                    String entityUUID = args.checkjstring(1); // Get UUID as a string
+                    LuaValue dataTable = args.checktable(2);
+
+                    // Retrieve the entity using the UUID
+                    Entity entity = Entities.getEntityByUUID(entityUUID);
+                    if (entity == null) {
+                        plugin.getLastSender().sendMessage("Entity with the given UUID not found.");
+                        return LuaValue.NIL;
+                    }
+
+                    // Modify custom name
+                    LuaValue customName = dataTable.get("customName");
+                    if (!customName.isnil()) {
+                        entity.customName(Component.text(customName.tojstring()));
+                    }
+
+                    // Modify health
+                    LuaValue health = dataTable.get("health");
+                    if (!health.isnil() && entity instanceof LivingEntity) {
+                        ((LivingEntity) entity).setHealth(health.todouble());
+                    }
+
+                    // Modify position
+                    LuaValue position = dataTable.get("position");
+                    if (!position.isnil() && position.istable()) {
+                        double x = position.get("x").todouble();
+                        double y = position.get("y").todouble();
+                        double z = position.get("z").todouble();
+                        Location newLocation = new Location(entity.getWorld(), x, y, z);
+                        entity.teleport(newLocation);
+                    }
+
+                    // Modify charged state (for Creepers)
+                    if (entity instanceof Creeper) {
+                        LuaValue charged = dataTable.get("charged");
+                        if (!charged.isnil()) {
+                            ((Creeper) entity).setPowered(charged.toboolean());
+                        }
+                    }
+
+                    // Modify isBaby state (for Ageable entities)
+                    LuaValue isBaby = dataTable.get("isBaby");
+                    if (!isBaby.isnil() && entity instanceof Ageable) {
+                        if (isBaby.toboolean()) {
+                            ((Ageable) entity).setBaby();
+                        } else {
+                            ((Ageable) entity).setAdult();
+                        }
+                    }
+                } else {
+                    plugin.getLastSender().sendMessage("You must provide a valid entity UUID and a Lua table.");
                 }
                 return LuaValue.NIL;
             }
--- src/main/resources/sumneko-docs.lua
+++ src/main/resources/sumneko-docs.lua
@@ -23,9 +23,16 @@ function luacraft.colorize(color, text) end
 --- Pauses the execution for a specified number of seconds.
 --- @param seconds number The duration to wait in seconds.
 --- ```lua
---- luacraft.wait(5) -- Waits for 5 seconds.
+--- luacraft.wait(5) -- Waits for 5 seconds without blocking other processes.
 --- ```
 function luacraft.wait(seconds) end
+
+--- Blocks execution for a specified number of seconds.
+--- @param seconds number The duration to sleep in seconds.
+--- ```lua
+--- luacraft.sleep(2.5) -- Blocks execution for 2.5 seconds.
+--- ```
+function luacraft.sleep(seconds) end
 
 --- Executes a command as the last command sender.
 --- Only players can run commands.
@@ -45,9 +52,9 @@ function luacraft.runcommand(command) end
 function luacraft.runCommand(command) end
 
 --- Applies a new position to the executing player.
---- @param x integer
---- @param y integer
---- @param z integer
+--- @param x integer The new X coordinate.
+--- @param y integer The new Y coordinate.
+--- @param z integer The new Z coordinate.
 --- ```lua
 --- luacraft.setPlayerPosition(0, 120, 0)
 --- ```
@@ -60,3 +67,50 @@ function luacraft.setPlayerPosition(x, y, z) end
 --- print(vec3.x, vec3.y, vec3.z)
 --- ```
 function luacraft.getPlayerPosition() end
+
+--- Summons an entity at the specified coordinates.
+--- The entity is identified by its Enum name.
+--- @param entityName string The name of the entity (as a string) to summon (e.g., "CREEPER", "ZOMBIE").
+--- @param x number The X coordinate where the entity should be summoned.
+--- @param y number The Y coordinate where the entity should be summoned.
+--- @param z number The Z coordinate where the entity should be summoned.
+--- ```lua
+--- -- Summons a creeper at (100, 64, 100)
+--- luacraft.summonAtPosition("CREEPER", 100, 64, 100)
+---
+--- -- Summons a skeleton at the player's current position
+--- local pos = luacraft.getPlayerPosition()
+--- luacraft.summonAtPosition("SKELETON", pos.x, pos.y, pos.z)
+--- ```
+function luacraft.summonAtPosition(entityName, x, y, z) end
+
+--- Retrieves all entities in the player's world as a JSON string.
+--- @return string @A JSON array containing information about all entities, where each entry has:
+---   - `id`: string (The UUID of the entity)
+---   - `type`: string (The type of the entity)
+---   - `position`: table (A table containing the x, y, z coordinates of the entity's location)
+--- ```lua
+--- local allEntities = luacraft.getAllEntities()
+--- print(allEntities) -- Outputs a JSON string of all entities
+--- ```
+function luacraft.getAllEntities() end
+
+--- Modifies an entity's properties based on a table of attributes.
+--- @param entityUUID string The UUID of the entity to be modified.
+--- @param modifications table A table containing the properties to modify. Possible keys:
+---   - `customName`: string (The new name for the entity).
+---   - `health`: number (New health value for the entity, only for living entities).
+---   - `position`: table (A table containing x, y, z coordinates for the entity's new location).
+---   - `charged`: boolean (Sets whether a creeper should be charged or not).
+---   - `isBaby`: boolean (Sets whether an ageable entity should be a baby or an adult).
+--- ```lua
+--- local mods = {
+---     customName = "Boss Monster",
+---     health = 100,
+---     position = {x = 200, y = 80, z = -300},
+---     charged = true,
+---     isBaby = false
+--- }
+--- luacraft.modifyEntityData("entity-uuid", mods)
+--- ```
+function luacraft.modifyEntityData(entityUUID, modifications) end

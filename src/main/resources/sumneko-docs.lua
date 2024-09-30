30974512027b3768e6cd0c773eeb2a5cff5d6ded
dankfmemes <dankfmemes@gmail.com>
2024-09-29 22:04:43 -0700

    update sumneko-docs

--- src/main/resources/sumneko-docs.lua
+++ src/main/resources/sumneko-docs.lua
@@ -33,4 +33,30 @@ function luacraft.wait(seconds) end
 --- ```lua
 --- luacraft.runcommand("/give @p diamond 1")
 --- ```
+--- @deprecated
 function luacraft.runcommand(command) end
+
+--- Executes a command as the last command sender.
+--- Only players can run commands.
+--- @param command string The command to be executed.
+--- ```lua
+--- luacraft.runCommand("/give @p diamond 1")
+--- ```
+function luacraft.runCommand(command) end
+
+--- Applies a new position to the executing player.
+--- @param x integer
+--- @param y integer
+--- @param z integer
+--- ```lua
+--- luacraft.setPlayerPosition(0, 120, 0)
+--- ```
+function luacraft.setPlayerPosition(x, y, z) end
+
+--- Retrieves the position of the executing player.
+--- @return { x: integer, y: integer, z: integer }
+--- ```lua
+--- local vec3 = luacraft.getPlayerPosition()
+--- print(vec3.x, vec3.y, vec3.z)
+--- ```
+function luacraft.getPlayerPosition() end

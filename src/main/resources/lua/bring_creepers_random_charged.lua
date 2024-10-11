323c1cac0476fbf6e7bc0b8a2761c7dbdacad20c
dankfmemes <dankfmemes@gmail.com>
2024-10-10 23:43:54 -0700

    update scripts

--- src/main/resources/lua/bring_creepers_random_charged.lua
+++ src/main/resources/lua/bring_creepers_random_charged.lua
@@ -2,7 +2,7 @@ local playerPos = LuaCraft.getLocalPlayer().getPosition()
 local allEntities = LuaCraft.getAllEntities()
 
 for i, entity in ipairs(allEntities) do
-    if entity.getType == 'CREEPER' then
+    if entity.type == 'CREEPER' then
         entity.setPosition(playerPos.x, playerPos.y, playerPos.z)
         LuaCraft.modifyEntityData(entity.getUUID(), {
             charged = math.random(1, 2) == 1
--- src/main/resources/lua/bring_zombies_random_babies.lua
+++ src/main/resources/lua/bring_zombies_random_babies.lua
@@ -2,7 +2,7 @@ local playerPos = LuaCraft.getLocalPlayer().getPosition()
 local allEntities = LuaCraft.getAllEntities()
 
 for i, entity in ipairs(allEntities) do
-    if entity.getType() == 'ZOMBIE' then
+    if entity.type == 'ZOMBIE' then
         entity.setPosition(playerPos.x, playerPos.y, playerPos.z)
         LuaCraft.modifyEntityData(entity.getUUID(), {
             isBaby = math.random(1, 2) == 1

73a8d949d1bbd32c554ccf814c44321fee058cc9
dankfmemes <dankfmemes@gmail.com>
2024-10-14 18:15:02 -0700

    return actual lua item

--- src/main/java/com/shawnjb/luacraft/lib/NewItem.java
+++ src/main/java/com/shawnjb/luacraft/lib/NewItem.java
@@ -12,6 +12,7 @@ import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.VarArgFunction;
 
 import com.shawnjb.luacraft.LuaCraft;
+import com.shawnjb.luacraft.LuaCraftItem;
 import com.shawnjb.luacraft.LuaCraftPlayer;
 import com.shawnjb.luacraft.utils.TextFormatter;
 
@@ -31,8 +32,6 @@ public class NewItem extends VarArgFunction {
 
     @Override
     public Varargs invoke(Varargs args) {
-        plugin.getLogger().info("createItem called");
-
         String materialName = args.checkjstring(1);
         LuaValue itemData = args.checktable(2);
         LuaValue playerLua = itemData.get("player");
@@ -42,47 +41,36 @@ public class NewItem extends VarArgFunction {
                 : null;
 
         if (player != null) {
-            plugin.getLogger().info("Player found: " + player.getName());
-
             Material material = Material.getMaterial(materialName.toUpperCase());
             if (material != null) {
-                plugin.getLogger().info("Material found: " + materialName);
-
                 ItemStack itemStack = new ItemStack(material, 1);
                 ItemMeta meta = itemStack.getItemMeta();
 
                 LuaValue customName = itemData.get("name");
                 if (customName.isstring()) {
-                    plugin.getLogger().info("Setting custom name: " + customName.tojstring());
                     Component coloredName = TextFormatter.toHexColors(customName.tojstring());
                     meta.displayName(coloredName);
                 }
 
                 LuaValue loreTable = itemData.get("lore");
                 if (loreTable.istable()) {
-                    plugin.getLogger().info("Setting lore");
                     List<Component> loreList = new ArrayList<>();
                     LuaValue loreKey = LuaValue.NIL;
                     while ((loreKey = loreTable.next(loreKey).arg1()).isnil() == false) {
                         String loreLine = loreTable.get(loreKey).tojstring();
                         Component coloredLoreLine = TextFormatter.toHexColors(loreLine);
                         loreList.add(coloredLoreLine);
-                        plugin.getLogger().info("Lore line added: " + loreLine);
                     }
                     meta.lore(loreList);
                 }
 
                 LuaValue enchantmentsTable = itemData.get("enchantments");
                 if (enchantmentsTable.istable()) {
-                    plugin.getLogger().info("Applying enchantments");
                     LuaValue enchantKey = LuaValue.NIL;
                     while ((enchantKey = enchantmentsTable.next(enchantKey).arg1()).isnil() == false) {
                         LuaValue enchantmentData = enchantmentsTable.get(enchantKey);
                         String enchantmentName = enchantmentData.get(1).tojstring();
                         int level = enchantmentData.get(2).toint();
-
-                        plugin.getLogger()
-                                .info("Processing enchantment: " + enchantmentName + " with level " + level);
 
                         Registry<Enchantment> enchantmentRegistry = RegistryAccess.registryAccess()
                                 .getRegistry(RegistryKey.ENCHANTMENT);
@@ -92,10 +80,8 @@ public class NewItem extends VarArgFunction {
 
                         if (enchantment != null) {
                             meta.addEnchant(enchantment, level, true);
-                            plugin.getLogger().info("Enchantment applied: " + enchantmentName);
                         } else {
                             plugin.getLastSender().sendMessage("Invalid enchantment: " + enchantmentName);
-                            plugin.getLogger().warning("Invalid enchantment: " + enchantmentName);
                         }
                     }
                 } else {
@@ -106,7 +92,8 @@ public class NewItem extends VarArgFunction {
                 player.getInventory().addItem(itemStack);
                 plugin.getLogger().info("Item added to player: " + player.getName());
                 plugin.getLogger().info("Item Meta: " + itemStack.getItemMeta().toString());
-                return LuaValue.userdataOf(itemStack);
+
+                return new LuaCraftItem(itemStack, plugin).toLuaValue();
             } else {
                 plugin.getLastSender()
                         .sendMessage(TextFormatter.toComponent("Invalid material: " + materialName));
